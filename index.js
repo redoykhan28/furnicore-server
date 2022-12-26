11a7afb2b4099bbe8d04a6714527df7e1b73e283
unknown <redoyk2015@gmail.com>
2022-12-26 18:30:14 +0600

    categories api added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 
 //middleware 
@@ -13,6 +15,37 @@ app.get('/', (req, res) => {
 
     res.send('Furnicore Running on Server')
 })
+
+//useing mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ytkvvxy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+
+        //create collection of categories
+        const categoriesCollection = client.db('Furnicore').collection('categories')
+
+        //get te categories
+        app.get('/categories', async (req, res) => {
+
+            const query = {}
+            const result = await categoriesCollection.find(query).toArray()
+            res.send(result)
+        })
+
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
+
+
 
 app.listen(port, () => {
 
